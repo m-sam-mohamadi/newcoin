63c06f31d24ef3e81309fc0469b747eeb5ed1ed1
m.sam <64077684+m-sam-mohamadi@users.noreply.github.com>
2023-04-08 16:09:11 +0430

    committed

---
+++ components/slider.vue
@@ -0,0 +1,63 @@
+<template>
+  <!-- =====START:own slider component===== -->
+  <div class="sliderContainer">
+    <v-btn class="slideLeft" @click="prevSlide()" icon
+      ><v-icon>lni lni-chevron-right-circle</v-icon></v-btn
+    >
+    <div class="slides">
+      <slot />
+    </div>
+    <v-btn class="slideRight" @click="nextSlide()" icon
+      ><v-icon>lni lni-chevron-left-circle</v-icon></v-btn
+    >
+  </div>
+  <!-- =====END:own slider component===== -->
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      i: 150,
+    };
+  },
+  methods: {
+    //scrolling to next slide method
+    nextSlide() {
+      const slider = document.querySelector(".slides") || undefined;
+      const cardWidth: number = Number(
+        document.querySelector(".cardComponent")?.clientWidth
+      );
+      if (slider == undefined) return;
+      slider!.scrollLeft -= cardWidth;
+    },
+    //scrolling to previous slide method
+    prevSlide() {
+      const slider = document.querySelector(".slides") || undefined;
+      const cardWidth: number = Number(
+        document.querySelector(".cardComponent")?.clientWidth
+      );
+      if (!slider) return;
+      slider!.scrollLeft += cardWidth;
+    },
+  },
+  mounted() {
+    //scroll slide when mouse wheeling
+    const slider = document.querySelector(".slides") || undefined;
+    const cardWidth: number = Number(
+      document.querySelector(".cardComponent")?.clientWidth
+    );
+    slider?.addEventListener("wheel", (e) => {
+      if (e.deltaY > 0) {
+        slider!.scrollLeft -= cardWidth;
+      } else {
+        slider!.scrollLeft += cardWidth;
+      }
+    });
+  },
+};
+</script>
+
+<style lang="scss">
+@import "~@/assets/scss/components/slider.scss";
+</style>
---
+++ pages/dashboard/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <client-only placeholder="loading...">
+    <dashboard-component />
+  </client-only>
+</template>
+
+<script>
+import dashboardComponent from "@/components/dashboard/dashboard.vue";
+export default {
+  components: {
+    dashboardComponent,
+  },
+};
+</script>
+
+<style></style>
